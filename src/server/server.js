dbbe7a682db586a00b707a1d4052646c0c764022
Ivan Ken Weng Chee <xenzaki@msn.com>
2018-05-05 15:13:16 +1000

    Updated flow of appointment creation

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ var db      = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'password',
-  database : 'mydb',
+  database : 'groomie',
   port     : 3306
 });
 
@@ -34,23 +34,59 @@ io.on('connection', function(socket) {
   console.log('connection established');
 
   socket.on('register', function(data) {
-    console.log('register')
+    console.log('register');
   });
 
   socket.on('login', function(data) {
-    console.log('login')
+    console.log('login');
   });
 
   socket.on('logout', function(data) {
-    console.log('logout')
+    console.log('logout');
+  });
+
+  socket.on('appointment list', function(data) {
+    console.log(data);
+    socket.emit('appointment list success', {
+      times: [
+        '09:00 - 10:30',
+        '09:30 - 11:00',
+        '10:00 - 11:30',
+        '10:30 - 12:00',
+        '11:00 - 12:30',
+        '11:30 - 13:00',
+        '12:00 - 13:30',
+        '12:30 - 14:00',
+        '13:00 - 14:30',
+        '13:30 - 15:00',
+        '14:00 - 15:30',
+        '14:30 - 16:00',
+        '15:00 - 16:30',
+        '15:30 - 17:00'
+      ],
+      dogs: [
+        'dog1',
+        'dog2',
+        'dog3'
+      ],
+      options: [
+        'wash only',
+        'wash and nail clipping',
+        'deluxe grooming'
+      ]
+    });
+  });
+
+  socket.on('appointment create', function(data) {
+    console.log(data);
   });
 
   socket.on('disconnect', function() {
-    console.log('connection terminated')
+    console.log('connection terminated');
   });
 
   socket.on('shutdown', function() {
-    console.log('server shutting down')
+    console.log('server shutting down');
     io.close();
     socket.close();
     db.end(function(err) {
